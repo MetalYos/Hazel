1ffe0e7cecd5c9aa7e770854e60f57a27acea031
Yossi Cohen <metalyos@gmail.com>
2018-11-07 22:12:20 +0200

    Added Logger (using spdlog).

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -2,6 +2,7 @@
 
 // For Use by Hazel applications
 #include "Hazel/Application.h"
+#include "Hazel/Log.h"
 
 // ---Entry Point---------------------
 #include "Hazel/EntryPoint.h"
--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -6,6 +6,12 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	// For now (will change)
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized Log!");
+	int a = 7;
+	HZ_INFO("Hello! Var={0}", a);
+
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
